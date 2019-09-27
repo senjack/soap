e4cc29624e9f4d2e17e0e117d09f0d8b3c8da490
Irenyak1 <inyakate@gmail.com>
2019-09-27 16:27:37 +0300

    Add AuthIndex and apply components

---
+++ src/components/apply.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import AuthIndex from "./AuthIndex"
+
+function apply() {
+  return (
+    <div>
+      <AuthIndex />
+    </div>
+  );
+}
+
+export default apply;
