ac2101b88fc4fd4da6960d36fd2127cd75c65696
Irenyak1 <inyakate@gmail.com>
2019-10-01 16:33:59 +0300

    Some css

--- src/components/AuthForm1.js
+++ src/components/AuthForm1.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../App.css";
 import { Link } from "react-router-dom";
 
+
 class AuthForm1 extends React.Component {
   state = { title: undefined };
 
