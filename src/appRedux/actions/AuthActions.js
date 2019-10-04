fe518ee1258a4284017da50af3515c8632aded48
Irenyak1 <inyakate@gmail.com>
2019-10-04 15:08:43 +0300

    Add reset and toggle

--- src/appRedux/actions/AuthActions.js
+++ src/appRedux/actions/AuthActions.js
@@ -1,19 +1,23 @@
-export const Login = "successfully loged in";
-export const Signup = "successfully signed up";
+// export const Login = "successfully loged in";
+// export const Signup = "successfully signed up";
 
-export const login = () => {
-    return {
-        type:Login
-    };
+// export const login = () => {
+//     return {
+//         type:Login
+//     };
 
-}
+// }
 
-export const signup = () => {
-    return {
-        type:Signup
-    };
+// export const signup = () => {
+//     return {
+//         type:Signup
+//     };
 
-}
+// }
+
+
+
+
 // const Login = {
 //     applicantLogin:{
 //         type,
