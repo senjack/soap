8b74c2f6fdd243d9705aa45fff2954947cfe38cf
Irenyak1 <inyakate@gmail.com>
2019-10-04 10:38:07 +0300

    Commit after changes in AuthButton1 and UserLoginReducer

--- src/components/AuthButton1.js
+++ src/components/AuthButton1.js
@@ -1,44 +1,43 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 
 const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-    textTransform: "capitalize"
-
-  },
-  input: {
-    display: 'none',
-  },
+    button: {
+        margin: theme.spacing(1),
+        textTransform: "capitalize"
+        
+    },
+    input: {
+        display: 'none',
+    },
 }));
 
 function ContainedButtons(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div>
-      <Button id="auth-btn1" variant="contained" color="primary" className={classes.button} onClick={(e) => { e.preventDefault(); props.action === "login" ? props.login() : props.action === "signup" ? props.signup() : console.error('error occured'); }}>
-        {props.caption}</Button>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Button id="auth-btn1" variant="contained" color="primary" className={classes.button} onClick={(e)=>{e.preventDefault();props.action === "login" ? props.login():props.action === "signup" ? props.signup():console.error('error occured');}}>
+                {props.caption}</Button>
+                </div>
+ )}
 
 //  to be put by Lambert
-const mapStateToProps = (appState) => {
+ const mapStateToProps = (appState) =>{
   console.log(appState)
-  return { appState: appState };
-}
-
-//  to be put by Irene
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: () => { dispatch({ type: 'login' }) },
-    signup: () => { dispatch({ type: 'signup' }) }
+    return {appState:appState};
   }
-}
+
+  //  to be put by Irene
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      login: ()=>{ dispatch({type: 'login'}) },
+      signup: ()=>{ dispatch({type: 'signup'}) }      
+    }
+  }
 
 //   Connect the 
-export default connect(mapStateToProps, mapDispatchToProps)(ContainedButtons);
+  export default connect(mapStateToProps,mapDispatchToProps)(ContainedButtons);
